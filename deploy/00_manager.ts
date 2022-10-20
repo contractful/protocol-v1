e57af57d3e03098b015f240285b7f6c3f369a134
Mijail Piekarz <piekarzmijail@gmail.com>
2022-10-20 19:16:13 -0300

    Add deploy script
    - Add the 'constants' file necessary for deploying the 'manager' contract.

---
+++ deploy/00_manager.ts
@@ -0,0 +1,49 @@
+import debugModule from 'debug';
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {Manager} from '../typechain';
+
+import {CHALLENGE_DURATION} from './constants';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const log = debugModule('deploy-setup');
+  log.enabled = true;
+  //   eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const {deployments} = hre as any;
+  const {ethers, getNamedAccounts, network} = hre;
+
+  // keep ts support on hre members
+  const {deployer, governance} = await getNamedAccounts();
+  const {deploy, catchUnknownSigner} = deployments;
+
+  log('Governance: ' + governance);
+  log('Deployer: ' + deployer);
+
+  // Deploy proxy
+  await catchUnknownSigner(
+    deploy('Manager', {
+      contract: 'Manager',
+      from: deployer,
+      proxy: {
+        owner: governance,
+        proxy: true,
+        execute: {
+          init: {
+            methodName: 'initialize',
+            args: [CHALLENGE_DURATION, governance],
+          },
+        },
+      },
+      log: true,
+    })
+  );
+
+  const ManagerDeployer = <Manager>(
+    await ethers.getContract('Manager', deployer)
+  );
+
+  // Print all contracts info pretty
+  log('Manager proxy address: ' + ManagerDeployer.address);
+};
+export default func;
+func.tags = ['all', 'manager', 'test', 'local', 'production'];
---
+++ deploy/constants.ts
@@ -0,0 +1,2 @@
+export const ONE_HOUR = 3600;
+export const CHALLENGE_DURATION = ONE_HOUR * 24 * 7;
